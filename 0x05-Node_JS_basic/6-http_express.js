90b148bb1db94e549a775e4f306c7c371612c4ce
iheb <ihebtimoumi12@gmail.com>
2024-07-23 19:18:54 +0100

    http express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 1245;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
