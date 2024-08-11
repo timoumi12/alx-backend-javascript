df4164b3ecd15394b972858f978a41f6020be7a3
iheb <ihebtimoumi12@gmail.com>
2024-08-11 18:08:28 +0100

    calcul1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -6,7 +6,7 @@ function calculateNumber(type, a, b) {
     switch(type) {
         case "SUM":
             return roundedA + roundedB;
-        case "SUBSTRACT":
+        case "SUBTRACT":
             return roundedA - roundedB;
         case "DIVIDE":
             if (roundedB === 0) {
